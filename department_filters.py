20dea75269fef2a9661e6f556c08a7531d054b56
Peter Rolfe <30848697+PeteyWoufes@users.noreply.github.com>
2019-02-14 17:21:55 +0000

    department_filters now commented.

--- department_filters.py
+++ department_filters.py
@@ -1,43 +1,36 @@
-
 import autofilter.google_api as google
 import autofilter.create_label as af_labels
 import autofilter.create_filter as filters
 import autofilter.group_manager as groups
 import autofilter.fetch_label_ids as fetch_labels
 import json
-import argparse
 
 
 def main():
+    ''' Loads config data. '''
     data = loadData()
     try:
         for user in data["users"]:
+            ''' Creates single label for a user. '''
             af_labels.add(user, "Pipeline")
+            ''' Gets existing labels for user's Gmail account. '''
             labels = fetch_labels.fetch_labels(google, user)
-            obj = filters.create_filter_object({"from": "pipeLine@giant.ie"}, labels, "Pipeline")
+            ''' Creates filter object using criteria provided by you, labels fetched earlier and a label name. '''
+            obj = filters.create_filter_object(
+                {"from": "pipeLine@giant.ie"}, labels, "Pipeline")
+            ''' Adds filter to the user's Gmail account. '''
             filters.add(user, obj)
         print("All done.")
-    except KeyError:
-        print(KeyError)
+    except:
+        print("ERROR.")
 
 
 def loadData():
+    ''' Loads config file. '''
     with open("config.json", "r") as x:
         data = json.loads(x.read())
     return data
 
-
-'''
-def getArgs():
-    parser = argparse.ArgumentParser(
-        description='Get arguments for show and group to add user to.')
-    parser.add_argument('-s', '--show', type=str,
-                        help='show')
-    # parser.add_argument('-g', '--group', type=str,
-    #                     help='group')
-    args = parser.parse_args()
-    return args
-'''
 
 if __name__ == "__main__":
     main()
